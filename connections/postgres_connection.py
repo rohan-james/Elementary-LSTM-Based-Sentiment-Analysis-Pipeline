50850b395bb8a0e5657ecaed41cfa19f2950b75d
Rohan James <rohanjames@Rohans-MacBook-Air.local>
2025-07-20 11:43:28 +0530

    change postgres connection

--- connections/postgres_connection.py
+++ connections/postgres_connection.py
@@ -4,7 +4,7 @@ import os
 
 DB_NAME = os.getenv("DB_NAME", "sentiment_db")
 DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "d7fiwxxmlx")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "user_password")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
 
